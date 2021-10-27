6a0ff4009caa17b0ec8429c9d593049b02038ce9
OMP-49 <om@192.168.0.104>
2021-10-27 21:40:13 +0530

    Simple binary tree

---
+++ src/main/java/trees/Tree.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package trees;
+
+/**
+ * @author Om
+ *
+ */
+public class Tree {
+	int value;
+	Tree left;
+	Tree right;
+	
+	public Tree(int value) {
+		this.value= value;
+		left = null;
+		right = null;
+	}
+	
+}
